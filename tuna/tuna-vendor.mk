e52fe4dbddb1d36a1bf96b538acf08d9cc3094d8
=?UTF-8?q?=3D=3FUTF-8=3Fq=3FD=3D2E=3D20Andrei=3D20M=3DC4=3D83ce=3DC8?= =?UTF-8?q?=3D99=3F=3D?= <dmaces@nd.edu>
2016-06-10 12:04:44 -0400

    widevine: Convert blob copying to modules w/ strip & pack relocs
    tuna: based on https://github.com/Unlegacy-Android/proprietary_vendor_widevine/commit/e43c16355bc75b553bf7d66b91883263a67d4204
    
    Change-Id: I517c9c75dc4d6619a57137fc502087156322f9a2

--- tuna/tuna-vendor.mk
+++ tuna/tuna-vendor.mk
@@ -17,3 +17,11 @@
 # Pick up overlay for features that depend on non-open-source files
 
 $(call inherit-product, vendor/samsung/tuna/tuna-vendor-blobs.mk)
+
+PRODUCT_PACKAGES += \
+    libdrmwvmplugin \
+    libwvdrmengine \
+    libdrmmtkutil \
+    libwvdrm_L3 \
+    libwvm \
+    libWVStreamControlAPI_L3
