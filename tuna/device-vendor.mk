8f5b13e834f3271b155ba2c54a99b3a051cdde97
Kyle Repinski <repinski23@gmail.com>
2016-10-20 20:04:02 -0500

    tuna: Switch libs to modules rather than copied files.
    Change-Id: Ic48e9355e76c9e769c6940a693ee1d04c5acb99c

--- tuna/device-vendor.mk
+++ tuna/device-vendor.mk
@@ -19,6 +19,18 @@
 $(call inherit-product, vendor/samsung/tuna/tuna-vendor-blobs.mk)
 
 PRODUCT_PACKAGES += \
+    libfrsdk \
+    libinvensense_mpl \
+    libsec-ril.maguro \
+    gps.omap4.maguro \
+    lib_gs4dt.toro-common \
+    gps.omap4.toro-common \
+    libsec-ril_lte.toro \
+    libims.toro \
+    libims_jni.toro \
+    libsec-ril_lte.toroplus
+
+PRODUCT_PACKAGES += \
     libdrmwvmplugin \
     libwvdrmengine \
     libwvdrm_L3 \
