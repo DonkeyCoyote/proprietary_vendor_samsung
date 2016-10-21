e76a76071bb29f1f9c5a598f4ffa4b035690d432
Kyle Repinski <repinski23@gmail.com>
2016-10-20 20:04:02 -0500

    tuna: Switch libs to modules rather than copied files.
    Change-Id: Ic48e9355e76c9e769c6940a693ee1d04c5acb99c

--- tuna/tuna-vendor-blobs.mk
+++ tuna/tuna-vendor-blobs.mk
@@ -2,9 +2,7 @@
 PRODUCT_COPY_FILES += \
     vendor/samsung/tuna/common/proprietary/vendor/firmware/ducati-m3.bin:system/vendor/firmware/ducati-m3.bin \
     vendor/samsung/tuna/common/proprietary/vendor/firmware/mms144_ts_rev31.fw:system/vendor/firmware/mms144_ts_rev31.fw \
-    vendor/samsung/tuna/common/proprietary/vendor/firmware/mms144_ts_rev32.fw:system/vendor/firmware/mms144_ts_rev32.fw \
-    vendor/samsung/tuna/common/proprietary/vendor/lib/libfrsdk.so:system/vendor/lib/libfrsdk.so \
-    vendor/samsung/tuna/common/proprietary/vendor/lib/libinvensense_mpl.so:system/vendor/lib/libinvensense_mpl.so
+    vendor/samsung/tuna/common/proprietary/vendor/firmware/mms144_ts_rev32.fw:system/vendor/firmware/mms144_ts_rev32.fw
 
 # SMC
 PRODUCT_COPY_FILES += \
@@ -14,28 +12,16 @@ PRODUCT_COPY_FILES += \
 # maguro-specific files copied to /system/vendor/maguro
 PRODUCT_COPY_FILES += \
     vendor/samsung/tuna/maguro/proprietary/vendor/etc/sirfgps.conf:system/vendor/maguro/etc/sirfgps.conf \
-    vendor/samsung/tuna/maguro/proprietary/vendor/firmware/bcm4330.hcd:system/vendor/maguro/firmware/bcm4330.hcd \
-    vendor/samsung/tuna/maguro/proprietary/vendor/lib/hw/gps.omap4.so:system/vendor/maguro/lib/hw/gps.omap4.so \
-    vendor/samsung/tuna/maguro/proprietary/vendor/lib/libsec-ril.so:system/vendor/maguro/lib/libsec-ril.so
+    vendor/samsung/tuna/maguro/proprietary/vendor/firmware/bcm4330.hcd:system/vendor/maguro/firmware/bcm4330.hcd
 
 # toro and toroplus common files copied to /system/vendor/toro-common
 PRODUCT_COPY_FILES += \
     vendor/samsung/tuna/toro-common/proprietary/vendor/etc/sirfgps.conf:system/vendor/toro-common/etc/sirfgps.conf \
-    vendor/samsung/tuna/toro-common/proprietary/vendor/firmware/bcm4330.hcd:system/vendor/toro-common/firmware/bcm4330.hcd \
-    vendor/samsung/tuna/toro-common/proprietary/vendor/lib/hw/gps.omap4.so:system/vendor/toro-common/lib/hw/gps.omap4.so \
-    vendor/samsung/tuna/toro-common/proprietary/vendor/lib/lib_gsd4t.so:system/vendor/toro-common/lib/lib_gsd4t.so
-
-# toro-specific files copied to /system/vendor/toro
-# ".apk" renamed to ".kpa" to allow it to get through PRODUCT_COPY_FILES
-PRODUCT_COPY_FILES += \
-    vendor/samsung/tuna/toro/proprietary/vendor/lib/libsec-ril_lte.so:system/vendor/toro/lib/libsec-ril_lte.so
-
+    vendor/samsung/tuna/toro-common/proprietary/vendor/firmware/bcm4330.hcd:system/vendor/toro-common/firmware/bcm4330.hcd
 
 # toroplus-specific files copied to /system/vendor/toroplus
 PRODUCT_COPY_FILES += \
     vendor/samsung/tuna/toroplus/proprietary/vendor/app/BIP.kpa:system/vendor/toroplus/app/BIP.kpa \
     vendor/samsung/tuna/toroplus/proprietary/vendor/app/HiddenMenu.kpa:system/vendor/toroplus/app/HiddenMenu.kpa \
     vendor/samsung/tuna/toroplus/proprietary/vendor/app/SDM.kpa:system/vendor/toroplus/app/SDM.kpa \
-    vendor/samsung/tuna/toroplus/proprietary/vendor/app/SecPhone.kpa:system/vendor/toroplus/app/SecPhone.kpa \
-    vendor/samsung/tuna/toroplus/proprietary/vendor/lib/libsec-ril_lte.so:system/vendor/toroplus/lib/libsec-ril_lte.so
-
+    vendor/samsung/tuna/toroplus/proprietary/vendor/app/SecPhone.kpa:system/vendor/toroplus/app/SecPhone.kpa
